ff00695b9d795a72d8e02b69d7cd693258a57752
Vishay Bhatia <vishaybhatia@addictivegifts.com>
2016-05-15 19:33:31 +0400

    Added users index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get 'about' => 'pages#about'
 
-  resources :users , only: [:show]
+  resources :users , only: [:show, :index]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
