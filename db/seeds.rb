27665f2be7d7a623de3cd07668a9fc688d161c0a
Vishay Bhatia <vishaybhatia@addictivegifts.com>
2016-05-16 11:25:40 +0400

    Added db seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: 'vishay87', email: 'vishay87@gmail.com', password: '12345678', password_confirmation: '12345678')
+User.create(username: 'vishay88', email: 'vishay88@gmail.com', password: '12345678', password_confirmation: '12345678')
+User.create(username: 'vishay89', email: 'vishay89@gmail.com', password: '12345678', password_confirmation: '12345678')
+User.create(username: 'vishay90', email: 'vishay90@gmail.com', password: '12345678', password_confirmation: '12345678')
+User.create(username: 'vishay91', email: 'vishay91@gmail.com', password: '12345678', password_confirmation: '12345678')
+p 'Test users created'
